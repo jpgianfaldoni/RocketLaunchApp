03701e2c12cdf6a188b3bfc54230421bac340104
jpgianfaldoni <jpgianfaldoni@gmail.com>
2020-10-17 16:04:02 -0300

    add initial files

---
+++ my-app/src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Foguete from "./components/Foguete"
+import logo from './logo.svg';
+import './App.css';
+
+
+
+class App extends React.Component {
+
+    constructor(){
+      super()
+      this.state = {
+        loading: true,
+        rocketData : {}
+      }
+    }
+
+
+      componentDidMount() {
+        this.setState({loading:true})
+        fetch("https://launchlibrary.net/1.4/launch/next/20")
+          .then(response => response.json())
+          .then(data => {
+            this.setState({
+              loading: false,
+              rocketData : data,
+            })
+          })
+        }
+      
+
+
+  render(){
+    const elements = []
+		var i
+		for (i = 0; i < 11; i++) {
+  			elements.push(<Foguete id = {i} rocketInfo = {this.state.rocketData} />);
+			}
+    return(
+      <div>
+				{
+      this.state.loading ? <h1>Loading</h1> :
+      <div className="App">
+      {elements}
+      </div>
+  }
+      </div>
+    )
+  }
+}
+
+export default App;
