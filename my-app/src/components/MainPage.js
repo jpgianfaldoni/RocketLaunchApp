02f98486a5818ca5acb4f937208b6824bfdce54e
jpgianfaldoni <jpgianfaldoni@gmail.com>
2020-10-31 16:00:18 -0300

    add checkboxes

--- my-app/src/components/MainPage.js
+++ my-app/src/components/MainPage.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import RocketCard from "./RocketCard"
 
+
 import { withStyles } from "@material-ui/core/styles";
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
+
 
 
 
@@ -46,147 +47,163 @@ const useStyles = theme => ({
 
 class MainPage extends React.Component {
 
-    constructor(){
-      super()
-      this.state = {
-        loading: true,
-        page : 0,
-        rocketData : {},
-        rocketName : "",
-        missionName : "",
-        rocketStatus: "",
-        agencies : ""
+  constructor() {
+    super()
+    this.state = {
+      loading: true,
+      page: 0,
+      rocketData: {},
+      rocketName: "",
+      missionName: "",
+      rocketStatus: "",
+      agencies: "",
+      nameList: {}
+    }
+    this.nextPageButton = this.nextPageButton.bind(this)
+    this.previousPageButton = this.previousPageButton.bind(this)
+    this.handleSubmitRocketName = this.handleSubmitRocketName.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.checkboxHandler = this.checkboxHandler.bind(this)
+    
+  }
+
+
+  componentDidMount() {
+    this.setState({ loading: true })
+    fetch("https://space-launch-db.herokuapp.com/filter?page=" + this.state.page + "&rocketName=" + this.state.rocketName + "&rocketStatus=" +
+      this.state.rocketStatus + "&missionName=" + this.state.missionName + "&agencies=" + this.state.agencies)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          rocketData: data,
+        })
+      })
+
+      fetch("https://space-launch-db.herokuapp.com/listAgencyName").then(response => response.json()).then(data => {this.setState({ nameList: data , loading: false})});
+  }
+
+
+  nextPageButton(event) {
+    event.preventDefault()
+    fetch("https://space-launch-db.herokuapp.com/filter?page=" + (this.state.page + 1) + "&rocketName=" + this.state.rocketName +
+      "&rocketStatus=" + this.state.rocketStatus + "&missionName=" + this.state.missionName + "&agencies=" + this.state.agencies)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          loading: false,
+          rocketData: data,
+          page: this.state.page + 1
+        })
+        console.log(this.state.nameList)
+      })
+  }
+
+  previousPageButton(event) {
+    event.preventDefault()
+    fetch("https://space-launch-db.herokuapp.com/filter?page=" + (this.state.page - 1) + "&rocketName=" + this.state.rocketName +
+      "&rocketStatus=" + this.state.rocketStatus + "&missionName=" + this.state.missionName + "&agencies=" + this.state.agencies)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          loading: false,
+          rocketData: data,
+          page: this.state.page - 1
+        })
+      })
+  }
+
+  handleSubmitRocketName(event) {
+    event.preventDefault()
+    fetch("https://space-launch-db.herokuapp.com/filter?page=" + this.state.page + "&rocketName=" + this.state.rocketName +
+      "&rocketStatus=" + this.state.rocketStatus + "&missionName=" + this.state.missionName + "&agencies=" + this.state.agencies)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          loading: false,
+          rocketData: data,
+        })
+      })
+  }
+
+
+  handleChange(event, state) {
+    const { value } = event.target
+    this.setState({
+      [state]: value,
+    })
+  }
+
+  checkboxHandler(event) {
+    var selValue = event.target.value;
+    var currState = this.state.agencies;
+    var currStateList = currState.split(",");
+    if(currState.length == 0){
+      this.setState({agencies: selValue});
+    } else if(currStateList.includes(selValue)) {
+      var index = currStateList.indexOf(selValue);
+      currStateList.splice(index, 1);
+      var newState = currStateList[0];
+      for (let i = 1; i<currStateList.length; i++){
+        newState += "," + currStateList[i];
       }
-      this.nextPageButton = this.nextPageButton.bind(this)
-      this.previousPageButton = this.previousPageButton.bind(this)
-      this.handleSubmitRocketName = this.handleSubmitRocketName.bind(this)
-      this.handleChange = this.handleChange.bind(this)
-      
-      
+      if (!newState) {this.setState({agencies: ""});} else {this.setState({agencies: newState});}
+    } else {
+      this.setState({agencies: currState + "," + selValue});
+
+    }
+    
+  }
+
+
+
+  render() {
+    const { classes } = this.props;
+    const rocketElements = [];
+    const checkboxLines = [];
+    console.log(this.state.agencies)
+    for (let i = 0; i < this.state.rocketData.length; i++) {
+      rocketElements.push(<Grid item xs={'auto'}><RocketCard id={i} rocketInfo={this.state.rocketData[i]} /></Grid>);
     }
 
-
-      componentDidMount() {
-        this.setState({loading:true})
-        fetch("https://space-launch-db.herokuapp.com/filter?page=" + this.state.page + "&rocketName=" + this.state.rocketName + "&rocketStatus=" + 
-        this.state.rocketStatus + "&missionName=" + this.state.missionName + "&agencies=" + this.state.agencies ) 
-          .then(response => response.json())
-          .then(data => {
-            this.setState({
-              loading: false,
-              rocketData : data,
-            })
-          })
-        }
-
-
-        nextPageButton(event){
-          event.preventDefault()
-          fetch("https://space-launch-db.herokuapp.com/filter?page=" + (this.state.page + 1) + "&rocketName=" + this.state.rocketName + 
-          "&rocketStatus=" + this.state.rocketStatus + "&missionName=" + this.state.missionName + "&agencies=" + this.state.agencies)
-            .then(response => response.json())
-            .then(data => {
-              this.setState({
-                loading: false,
-                rocketData : data,
-                page : this.state.page + 1
-              })
-          })
-          console.log(this.state.page)
-        }
-
-        previousPageButton(event){
-          event.preventDefault()
-          fetch("https://space-launch-db.herokuapp.com/filter?page=" + (this.state.page -1) + "&rocketName=" + this.state.rocketName +
-           "&rocketStatus=" + this.state.rocketStatus + "&missionName=" + this.state.missionName + "&agencies=" + this.state.agencies)
-            .then(response => response.json())
-            .then(data => {
-              this.setState({
-                loading: false,
-                rocketData : data,
-                page : this.state.page - 1
-              })
-          })
-          console.log(this.state.page)
-        }
-
-        handleSubmitRocketName(event){
-          event.preventDefault()
-          fetch("https://space-launch-db.herokuapp.com/filter?page=" + this.state.page  + "&rocketName=" + this.state.rocketName + 
-          "&rocketStatus=" + this.state.rocketStatus + "&missionName=" + this.state.missionName + "&agencies=" + this.state.agencies)
-            .then(response => response.json())
-            .then(data => {
-              this.setState({
-                loading: false,
-                rocketData : data,
-              })
-              console.log("SUBMIT")
-              console.log(this.state.rocketName)
-              console.log(this.state.rocketStatus)
-            })
-        }
-      
-
-        handleChange(event, state){
-          const {value} = event.target
-          this.setState({
-            [state]: value,
-          })
-          console.log(this.state.rocketStatus)
-        }
-      
-
-
-  render(){
-    const {classes} = this.props;
-    const elements = []
-		for (let i = 0; i < this.state.rocketData.length; i++) {
-        elements.push(<Grid item xs={4}><RocketCard id = {i} rocketInfo = {this.state.rocketData[i]} /></Grid>);
-			}
-    return(
-      <div>
-				{
-      this.state.loading ? <CircularProgress />:
-      
-      <div className={classes.buttonRoot}>
-      {
-        this.state.page > 0 ?
-        <Button variant="contained" color="primary" onClick = {(e)=> this.previousPageButton(e)}>
-          Previous
-        </Button> : <div></div>
+    if (!this.state.loading){
+      for (let i = 0; i < this.state.nameList.agencies.length; i++) {
+        checkboxLines.push(<FormControlLabel 
+            control={
+              <Checkbox value={this.state.nameList.agencies[i]} onChange={this.checkboxHandler}/>} 
+          label={this.state.nameList.agencies[i]}
+        />);
       }
-      <Button variant="contained" color="primary" onClick = {(e)=> this.nextPageButton(e)}>
-        Next
-      </Button>
-      <form className={classes.root} noValidate autoComplete="off" onSubmit = {(e)=> this.handleSubmitRocketName(e)}>
-        <TextField id="outlined-basic" label="Rocket Name" variant="outlined" value = {this.state.rocketName} onChange={(e) => this.handleChange(e, "rocketName")}/>
-        <TextField id="outlined-basic" label="Mission Name" variant="outlined" value = {this.state.missionName} onChange={(e) => this.handleChange(e, "missionName")}/>
-        <TextField id="outlined-basic" label="Agency" variant="outlined" value = {this.state.agencies} onChange={(e) => this.handleChange(e, "agencies")}/>
-        <FormControl variant="outlined" className={classes.formControl}>
-          <InputLabel id="demo-simple-select-autowidth-label">Status</InputLabel>
-          <Select
-            labelId="demo-simple-select-autowidth-label"
-            id="demo-simple-select-autowidth"
-            value={this.state.rocketStatus}
-            onChange={(e) => this.handleChange(e, "rocketStatus")}
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={"Success"}>Success</MenuItem>
-            <MenuItem value={"Failure"}>Failure</MenuItem>
+    }
 
-          </Select>
-      </FormControl>
-        <Button variant="contained" color="primary" type = "Submit">
-          Submit
+    return (
+      <div>
+        {
+        this.state.loading ? <CircularProgress /> :
+
+            <div className={classes.buttonRoot}>
+              {
+                this.state.page > 0 ?
+                  <Button variant="contained" color="primary" onClick={(e) => this.previousPageButton(e)}>
+                    Previous
+        </Button> : <div></div>
+              }
+              <Button variant="contained" color="primary" onClick={(e) => this.nextPageButton(e)}>
+                Next
         </Button>
-      </form>
-      <Grid container spacing={2}>
-        {elements}
-      </Grid>
-      </div>
-  }
+              <form className={classes.root} noValidate autoComplete="off" onSubmit={(e) => this.handleSubmitRocketName(e)}>
+                <TextField id="outlined-basic" label="Rocket Name" variant="outlined" value={this.state.rocketName} onChange={(e) => this.handleChange(e, "rocketName")} />
+                <Button variant="contained" color="primary" type="Submit">
+                  Submit
+        </Button>
+              </form>
+              <Grid container spacing={2}>
+                {rocketElements}
+              </Grid>
+        <div>
+            <h3>{checkboxLines}</h3>
+        </div>
+            </div>
+        }
       </div>
     )
   }
