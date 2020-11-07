c63222b14e8d56190e35a8103c8eaae09473d6ab
Carlos Dip <cadudip@hotmail.com>
2020-11-07 18:50:41 -0300

    Created server.js and removed build dir from gitignore

---
+++ build/precache-manifest.1453424727abe575e901615d155fdc47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d01a5bf2cf226b4e766145007aed94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e91ca51f8877b78250e3",
+    "url": "/static/css/main.0c4c89ca.chunk.css"
+  },
+  {
+    "revision": "307ede3128e4d6f35400",
+    "url": "/static/js/2.7eb8ed96.chunk.js"
+  },
+  {
+    "revision": "f91855487d10fee9da543aecf4a9397d",
+    "url": "/static/js/2.7eb8ed96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e91ca51f8877b78250e3",
+    "url": "/static/js/main.b7f9a68b.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
