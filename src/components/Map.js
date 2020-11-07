7edf229069b5036df6fc7cfb44ee99b50ec5566d
Carlos Dip <cadudip@hotmail.com>
2020-11-07 18:03:14 -0300

    Removed local key access.

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,7 +6,7 @@ import ReactMapGL from 'react-map-gl';
 
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-const key = require("../teste/teste") || process.env.MAPKEY;
+const key = process.env.MAPKEY;
 
 class RocketMap extends Component {
 
